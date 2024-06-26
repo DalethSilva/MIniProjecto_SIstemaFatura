478b92c88a139b564bd9bf69d51caf13323cfb2e
DalethSilva <153534514+DalethSilva@users.noreply.github.com>
2024-06-26 22:11:16 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,20 @@
 <?php
 $host = 'localhost';
-$db = 'faturacao';
+$db   = 'faturacao';
 $user = 'root';
 $pass = '';
+$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
 try {
-    $pdo = new PDO($dsn, $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Conexão falhou: ' . $e->getMessage();
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
