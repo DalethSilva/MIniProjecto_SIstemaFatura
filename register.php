60b1e6fa15df498d17ffbb16513eafd1b076d57a
DalethSilva <153534514+DalethSilva@users.noreply.github.com>
2024-06-26 22:16:20 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,16 +1,28 @@
 <?php
-include 'db.php';
+include 'database.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+try {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
 
-    $stmt = $pdo->prepare("INSERT INTO administradores (nome, email, senha) VALUES (?, ?, ?)");
-    if ($stmt->execute([$nome, $email, $senha])) {
-        echo "Administrador registrado com sucesso!";
+        $stmt = $pdo->prepare("SELECT * FROM administradores WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        if ($stmt->rowCount() > 0) {
+            echo "<script>alert('Email já registrado');</script>";
+            echo "<script>window.location.href = 'loginPage.html';</script>";
+        } else {
+            $stmt = $pdo->prepare("INSERT INTO administradores (nome, email, senha) VALUES (:nome, :email, :senha)");
+            $stmt->execute(['nome' => $nome, 'email' => $email, 'senha' => $senha]);
+
+            echo "<script>alert('Registro bem-sucedido! Você pode fazer login agora.');</script>";
+            echo "<script>window.location.href = 'loginPage.html';</script>";
+        }
     } else {
-        echo "Erro ao registrar administrador!";
+        echo "Método de requisição incorreto";
     }
+} catch (Exception $e) {
+    echo 'Erro: ' . $e->getMessage();
 }
 ?>
